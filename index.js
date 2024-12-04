b9b5fbe7681c081120fe595b741c97263cd4dcbf
akshatverma1 <akshaverma14@gmail.com>
2024-12-04 13:27:17 +0530

    Update

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express"); 
 const app = express();
 const port = 2;
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:true}));
+
 app.listen(port ,()=>{
     console.log("Server Start");
 })
